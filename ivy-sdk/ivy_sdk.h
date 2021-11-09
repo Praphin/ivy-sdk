858ef204a7157fa285f31c7d751be7810083c664
PSP <sppraphin@gmail.com>
2021-11-09 16:49:28 +0530

    Initial Commit

---
+++ ivy-sdk/ivy_sdk.h
@@ -0,0 +1,18 @@
+//
+//  ivy_sdk.h
+//  ivy-sdk
+//
+//  Created by Praphin SP on 09/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ivy_sdk.
+FOUNDATION_EXPORT double ivy_sdkVersionNumber;
+
+//! Project version string for ivy_sdk.
+FOUNDATION_EXPORT const unsigned char ivy_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ivy_sdk/PublicHeader.h>
+
+
